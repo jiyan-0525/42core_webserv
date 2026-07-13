c4dc9d042effc215cdbbc68eaf2898a225e20c38
Fiona-0525 <jiyanw825@gmail.com>
2026-07-13 21:20:45 +0200

    addconfigparser

--- includes/configParser.hpp
+++ includes/configParser.hpp
@@ -1,6 +1,17 @@
 #ifndef CONFIGPARSER_HPP
 # define CONFIGPARSER_HPP
 # include "Config.hpp"
+# include <string>
+# include <vector>
+
+class ConfigParser {
+public:
+    static std::vector<Config> parseConfigFile(const std::string& filename);
+
+private:
+    static void parserLine(const std::string& line, Config& config);
+    static void validate(const std::vector<Config>& configs);
+};
 
 
 #endif
