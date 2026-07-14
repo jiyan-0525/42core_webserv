9cf5193a25aa933620c5f2cd349f38c610252084
Jiyan Wang <jiyawang@2-E-4.42heilbronn.de>
2026-07-14 17:32:17 +0200

    addcontext

--- srcs/config/configParser.cpp
+++ srcs/config/configParser.cpp
@@ -1,1 +1,10 @@
 #include "ConfigParser.hpp"
+
+static std::vector<Config> ConfigParser::parseConfigFile(const std::string& filename) {
+    std::vector<Config> configs;
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open config file: " + filename);
+    }
+}
+
