1bc99bbe4f2d7f3504cb538934cb9aa4b1fbf851
Fiona-0525 <jiyanw825@gmail.com>
2026-07-14 21:04:23 +0200

    mofig

--- srcs/config/configParser.cpp
+++ srcs/config/configParser.cpp
@@ -1,10 +1,2 @@
 #include "ConfigParser.hpp"
 
-static std::vector<Config> ConfigParser::parseConfigFile(const std::string& filename) {
-    std::vector<Config> configs;
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("Could not open config file: " + filename);
-    }
-}
-
